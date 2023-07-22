a8b10e1c1c2e6d7c3d39614b623e9e091e2a705f
UzakoffUmar <uzakovumar338@gmail.com>
2023-07-22 06:35:49 +0500

    added experience

--- src/pages/About/Info/Experience.jsx
+++ src/pages/About/Info/Experience.jsx
@@ -25,6 +25,29 @@ const Experience = () => {
         </h5>
       </div>
       <div className="mx-5 mt-5 flex flex-col gap-3">
+        <h6 className="font-bold text-2xl">CoddyCamp IT Academy</h6>
+        <p className="font-thin uppercase">
+          {language === "uz"
+            ? "FRONTEND - MENTOR"
+            : language === "ru"
+            ? "Ментор - Frontend"
+            : "FRONTEND - MENTOR"}
+        </p>
+        <span
+          className={`flex flex-row items-center gap-2 font-thin text-sm opacity-60 ${
+            theme === "light" ? "text-dark2" : "text-light2"
+          }`}>
+          <FaCalendarAlt />
+          2023
+          {language === "uz"
+            ? " Iyul - Hozirda ishlamoqdaman"
+            : language === "ru"
+            ? " Июль - сейчас работаю"
+            : " July - Currently working"}
+        </span>
+        <hr className="w-40 h-1 my-3 bg-blue border-0 rounded"></hr>
+      </div>
+      <div className="mx-5 mt-5 flex flex-col gap-3">
         <h6 className="font-bold text-2xl">Mars IT School</h6>
         <p className="font-thin uppercase">
           {language === "uz"
@@ -38,12 +61,12 @@ const Experience = () => {
             theme === "light" ? "text-dark2" : "text-light2"
           }`}>
           <FaCalendarAlt />
-          2023 {" "}
+          2023{" "}
           {language === "uz"
-            ? "Iyun - Hozirda ishlamoqdaman"
+            ? "Iyun - 2023 Iyul"
             : language === "ru"
-            ? "Июнь - сейчас работаю"
-            : "June - Currently working"}
+            ? "Июнь - 2023 Июль"
+            : "June - 2023 July"}
         </span>
         <hr className="w-40 h-1 my-3 bg-blue border-0 rounded"></hr>
       </div>
